f7f6369b5891471bfdd02e744aa4d4a5d50964d1
Marco Cafasso <marcocafassog@gmail.com>
2023-02-15 23:32:45 +0100

    feat: implements ActionListener interface
    The listener interface for receiving action events. The class that is interested in processing an action event implements this interface, and the object created with that class is registered with a component, using the component's addActionListener method.

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,11 +3,19 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class Calculator {
-
+public class Calculator implements ActionListener{
+	
+	Calculator(){
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
