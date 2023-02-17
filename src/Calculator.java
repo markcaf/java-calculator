ad1f0bdafd17733cdcd5054e5b7e188baffa1491
Marco Cafasso <marcocafassog@gmail.com>
2023-02-17 14:17:40 +0100

    docs: add comments

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 
 public class Calculator implements ActionListener{
 	
+	// Variable declaration
 	JFrame frame;
 	JTextField textfield;
 	JButton[] numberButtons = new JButton[10];
@@ -18,18 +19,22 @@ public class Calculator implements ActionListener{
 	double num1=0,num2=0,result=0;
 	char operator;
 	
+	// Calculator constructor
 	Calculator(){
 		
+		// Frame creation
 		frame = new JFrame("Java Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(420, 550);
 		frame.setLayout(null);
 		
+		// Creating the textfield
 		textfield = new JTextField();
 		textfield.setBounds(50, 25, 300, 50);
 		textfield.setFont(myFont);
 		textfield.setEditable(false);
 		
+		// Creating buttons for mathematical operations
 		addButton = new JButton("+");
 		subButton = new JButton("-");
 		mulButton = new JButton("*");
@@ -40,6 +45,7 @@ public class Calculator implements ActionListener{
 		clrButton = new JButton("Clr");
 		negButton = new JButton("( - )");
 		
+		// Add buttons to array type variables
 		functionButtons[0] = addButton;
 		functionButtons[1] = subButton;
 		functionButtons[2] = mulButton;
@@ -50,12 +56,14 @@ public class Calculator implements ActionListener{
 		functionButtons[7] = clrButton;
 		functionButtons[8] = negButton;
 		
+		// Assignment of events, fonts and focusables for each function button in the array
 		for(int i=0; i<9; i++) {
 			functionButtons[i].addActionListener(this);
 			functionButtons[i].setFont(myFont);
 			functionButtons[i].setFocusable(false);
 		}
 		
+		// Assignment of values, events, fonts, focusables for each number button in the array
 		for(int i=0; i<10; i++) {
 			numberButtons[i] = new JButton(String.valueOf(i));
 			numberButtons[i].addActionListener(this);
@@ -63,14 +71,17 @@ public class Calculator implements ActionListener{
 			numberButtons[i].setFocusable(false);
 		}
 		
+		// Button size definition
 		delButton.setBounds(150, 430, 100, 50);
 		clrButton.setBounds(250, 430, 100, 50);
 		negButton.setBounds(50, 430, 100, 50);
 		
+		// Creating a panel to contain the button
 		panel = new JPanel();
 		panel.setBounds(50, 100, 300, 300);
 		panel.setLayout(new GridLayout(4, 4, 10, 10));
 		
+		// Add buttons to panel in grid order
 		panel.add(numberButtons[1]);
 		panel.add(numberButtons[2]);
 		panel.add(numberButtons[3]);
@@ -88,6 +99,7 @@ public class Calculator implements ActionListener{
 		panel.add(equButton);
 		panel.add(divButton);
 		
+		// Add components into the frame
 		frame.add(panel);
 		frame.add(negButton);
 		frame.add(delButton);
